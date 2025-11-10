fb07c664b6201b52c00b3c8b4f251c241d3306e2
Dssolved <lovelyfromhell@gmail.com>
2025-11-10 21:30:20 +0500

    first commit

--- src/main/java/com/example/games/repository/GameRepository.java
+++ src/main/java/com/example/games/repository/GameRepository.java
@@ -1,9 +1,11 @@
 package com.example.games.repository;
 
 import com.example.games.entity.Game;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@Transactional
 public interface GameRepository extends JpaRepository<Game, Long> {
 }
